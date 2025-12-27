cf69abb1dbfafaf8d2db04b8098bb30bac8e4c84
67kid-glitch <jcwright014@gmail.com>
2025-12-27 09:52:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-document.body.style.background = "red";
-console.log("MAIN JS RUNNING");
+console.log("main.js loaded");
+
+const overlay = document.getElementById("click");
+
+overlay.addEventListener("click", () => {
+  console.log("clicked");
+  overlay.style.display = "none";
+  document.body.style.background = "green";
+});
