34131c582be64390868196566864b7226ca3a4f9
67kid-glitch <jcwright014@gmail.com>
2025-12-27 11:05:34 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,27 @@
-const renderer = new Renderer();
+const structures = new StructureSystem(renderer.scene);
 
-let currentDimension;
+// Spawn structures
+structures.spawn("trialChamber", 10, 10);
+structures.spawn("woodlandMansion", -20, 10);
+structures.spawn("fortress", 15, -20);
+structures.spawn("bastion", -15, -20);
 
-function loadDimension(name) {
-  if (currentDimension) currentDimension.destroy();
+// Mob spawner
+const spawner = new Spawner(renderer.scene, "Zombie", 5, 5);
 
-  if (name === "nether") {
-    currentDimension = new Dimension("nether", renderer.scene, {
-      sky: 0x330000,
-      block: 0xaa0000
-    });
-  } else if (name === "end") {
-    currentDimension = new Dimension("end", renderer.scene, {
-      sky: 0x000000,
-      block: 0x555555
-    });
-  } else {
-    currentDimension = new Dimension("overworld", renderer.scene, {
-      sky: 0x87ceeb,
-      block: 0x55aa55
-    });
-  }
-}
+// Redstone
+const redstone = new Redstone();
+const lamp = new RedstoneLamp(renderer.scene, redstone, 0, 5);
 
-loadDimension("overworld");
-
-const player = new Player(renderer.camera);
-player.addToScene(renderer.scene);
-
-// === TEMP DIMENSION SWITCH KEYS ===
 window.addEventListener("keydown", e => {
-  if (e.key === "1") loadDimension("overworld");
-  if (e.key === "2") loadDimension("nether");
-  if (e.key === "3") loadDimension("end");
-});
-
-const overlay = document.getElementById("click");
-overlay.addEventListener("click", () => {
-  overlay.style.display = "none";
-  renderer.renderer.domElement.requestPointerLock();
+  if (e.key === "r") redstone.toggle();
 });
 
 function loop() {
   requestAnimationFrame(loop);
   player.update();
+  mobs.update(player, renderer.camera);
+  spawner.update(mobs);
+  lamp.update();
   renderer.render();
 }
-
-loop();
