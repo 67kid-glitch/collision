d56b4a5c9b001333c52ee8d760073056a1f6157f
67kid-glitch <jcwright014@gmail.com>
2025-12-27 13:44:54 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
-import { Game } from "./core/Game.js";
-import { World } from "./world/World.js";
-import { Player } from "./player/Player.js";
-import { Renderer } from "./engine/Renderer.js";
-import { MobSystem } from "./mobs/MobSystem.js";
-import { StructureSystem } from "./world/StructureSystem.js";
+// click to play
 const startScreen = document.getElementById("startScreen");
-
-startScreen.addEventListener("click", () => {
+startScreen.onclick = () => {
   document.body.requestPointerLock();
   startScreen.style.display = "none";
-});
+};
+
+// renderer
 const renderer = new Renderer();
-// === DEBUG TEST CUBE (DO NOT SKIP) ===
+
+// DEBUG CUBE (KEEP THIS FOR NOW)
 const testGeo = new THREE.BoxGeometry(5, 5, 5);
 const testMat = new THREE.MeshLambertMaterial({ color: 0xff0000 });
 const testCube = new THREE.Mesh(testGeo, testMat);
 testCube.position.set(0, 5, 0);
 renderer.scene.add(testCube);
+
+// world
 const world = new World(renderer.scene);
+
+// player
 const player = new Player(renderer.camera);
+
+// mobs
 const mobs = new MobSystem(renderer.scene);
-const structures = new StructureSystem(renderer.scene);
-
-structures.spawn("trialChamber", 10, 10);
-structures.spawn("woodlandMansion", -20, 10);
-structures.spawn("fortress", 20, -20);
-structures.spawn("bastion", -20, -20);
-
 mobs.setDimension("overworld");
 
-function gameLoop() {
-  requestAnimationFrame(gameLoop);
-
+// loop
+function loop() {
+  requestAnimationFrame(loop);
   player.update();
   mobs.update(player, renderer.camera);
   renderer.render();
 }
-
-gameLoop();
+loop();
