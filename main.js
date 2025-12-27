f87687a750293843f907502fffb9e47d441a724e
67kid-glitch <jcwright014@gmail.com>
2025-12-27 09:35:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const renderer = new Renderer();
 const world = new World(renderer.scene);
 const player = new Player(renderer.camera);
 
+const overlay = document.getElementById("click");
+
+overlay.addEventListener("click", () => {
+  overlay.style.display = "none";
+  renderer.renderer.domElement.requestPointerLock();
+});
+
 function loop() {
   requestAnimationFrame(loop);
   player.update();
