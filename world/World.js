b9bf60e42c54da4a47b13cd0e70ad5e1764ade6e
67kid-glitch <jcwright014@gmail.com>
2025-12-27 10:00:07 -0800

    Update World.js

--- world/World.js
+++ world/World.js
@@ -1,17 +1,13 @@
 class World {
   constructor(scene) {
-    this.scene = scene;
-    this.blocks = [];
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshStandardMaterial({ color: 0x55aa55 });
+    const geo = new THREE.BoxGeometry(1, 1, 1);
+    const mat = new THREE.MeshLambertMaterial({ color: 0x55aa55 });
 
     for (let x = -10; x <= 10; x++) {
       for (let z = -10; z <= 10; z++) {
-        const block = new THREE.Mesh(geometry, material);
+        const block = new THREE.Mesh(geo, mat);
         block.position.set(x, 0, z);
         scene.add(block);
-        this.blocks.push(block);
       }
     }
   }
